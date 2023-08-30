51ed55a0dae518e556b4243ab676637d61b9e0b6
Gabriel Cordova Medeiros <gabiceemi@gmail.com>
2023-08-30 10:08:07 -0300

    alter experiences

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,12 +20,14 @@ export default function About() {
         />
         <div className="max-w-sm px-8 py-8 text-left">
           <p className="font-sans">
-            Sou <b>Engenheiro de Software</b> com mais de 2 anos de experiência
-            na indústria. Tenho experiências como suporte, implantação e
-            desenvolvimento de <b>aplicações híbridas</b> e <b>cloud</b>,
-            participei desses projetos desde a sua concepção até a implantação,
-            além disso tenho grande entusiasmo pela área de <b>Design UI/UX</b>,
-            priorizando sempre a experiência e satisfação do usuário final.
+            Sou <b>Engenheiro de Software</b> com 3 anos de experiência na
+            concepção, desenvolvimento e implantação de projetos de software.
+            Comprometido com a excelência e a conformidade, busco uma posição
+            desafiadora como <b>Desenvolvedor</b>. Minhas habilidades técnicas,
+            capacidade de <b>resolução de problemas</b> e{' '}
+            <b>comunicação eficaz</b> garantem <b>códigos de alta qualidade</b>{' '}
+            e <b>satisfação do cliente</b>. Pronto para aplicar expertise em
+            diagnóstico e solução de problemas para fortalecer a equipe.
           </p>
         </div>
       </div>
@@ -36,24 +38,58 @@ export default function About() {
       </div>
       <div className="grid w-screen grid-cols-1 overflow-hidden px-10 py-5 md:grid-cols-3 md:px-52">
         <CardExp
-          title="Desenvolvedor Web   ( ) {"
-          description="Elicitação de requisitos, análise, projeto, desenvolvimento (back-end: Laravel, front-end: HTML, CSS, JS), testes e implantação em ambiente de produção."
-        />
-        <CardExp
-          title="Assistente de Suporte e Implantação  ( ) {"
-          description="Responsável por auxiliar na implantação de produtos na nuvem e fornecer suporte técnico."
+          title="Desenvolvedor FullStack  ( ) {"
+          description={
+            'Fevereiro de 2023 - Até o momento\n\n' +
+            'Entregando soluções completas de software, desde aplicativos a sites, com foco em alta qualidade e experiência do cliente. Responsável pelo desenvolvimento front-end e back-end, englobando a criação de interfaces intuitivas, funcionalidades avançadas e integração dinâmica.\n\n' +
+            'Gerenciando todos os aspectos do ciclo de vida do projeto, desde a concepção até a entrega final. Lidando diretamente com os clientes para entender requisitos, apresentar progresso e garantir alinhamento contínuo, demonstrando habilidades excepcionais de comunicação e gestão.\n\n' +
+            'Priorizando a satisfação do cliente, colaborando ativamente para entender as necessidades e superar expectativas, o que se traduz em feedback positivo e relacionamentos de longo prazo.\n\n' +
+            'Tecnologias: Docker, Jest, TypeScript, JavaScript, PostgreSql, Node.js, React Native, Adobe XD, Next.js, Flutter, Dart.\n\n'
+          }
         />
         <CardExp
           title="Desenvolvedor de Extensões  ( ) {"
-          description="Responsável por analisar, implementar e testar as extensões dos produtos na nuvem, garantindo a qualidade e o cumprimento dos prazos. Elaboração de documentação, manter o repositório organizado e fornecer suporte técnico."
+          description={
+            'Fevereiro de 2022 - Fevereiro de 2023\n\n' +
+            'Analisar, implementar e testar extensões cloud seguindo padrões de desenvolvimento e prazos;\n\n' +
+            'Garantir qualidade por meio de testes em condições simuladas;\n\n' +
+            'Elaborar documentação e manuais para extensões;\n\n' +
+            'Organizar e manter repositório das extensões da Central de Serviços;\n\n' +
+            'Fornecer suporte para extensões mantidas;\n\n' +
+            'Estimar esforço para desenvolvimento de extensões;\n\n' +
+            'Contribuir para padrões técnicos e evolução contínua.\n\n' +
+            'Tecnologias: Jaspersoft Studio, Java, PostgreSql, BFC Script, Selenium, REST Api.\n\n'
+          }
         />
         <CardExp
           title="Analista de Suporte e Implantação  ( ) {"
-          description="Prestar suporte e assistência técnica aos usuários, incluindo implantação de novos produtos e solução de pendências, fornecendo atendimentos e identificando soluções alternativas para aprimorar o atendimento ao cliente."
+          description={
+            'Fevereiro de 2021 - Fevereiro de 2022\n\n' +
+            'Auxiliar na implantação de produtos da linha cloud;\n\n' +
+            'Prestar atendimento de suporte e resolver pendências do cliente;\n\n' +
+            'Identificar e comunicar riscos aos coordenadores;\n\n'
+          }
         />
         <CardExp
-          title="Freelancer  ( ) {"
-          description="Especializado em web e mobile, responsável pela criação e implementação de soluções digitais personalizadas para atender às necessidades dos clientes."
+          title="Desenvolvedor Web   ( ) {"
+          description={
+            'Julho de 2020 - Janeiro de 2021\n\n' +
+            'Elicitação de requisitos de software para as novas funcionalidades, análise e projeto das novas funcionalidades identificadas;\n\n' +
+            'Desenvolvimento da aplicação Web;\n\n' +
+            'Testes de software da aplicação Web desenvolvida;\n\n' +
+            'Implantação da aplicação Web desenvolvida em uma máquina virtual (VM) para o ambiente de produção.\n\n' +
+            'Tecnologias: PHP, Laravel, MySQL, JavaScript, Bootstrap, CSS.\n'
+          }
+        />
+        <CardExp
+          title="Desenvolvedor Mobile  ( ) {"
+          description={
+            'Janeiro de 2020 -  Julho de 2020\n\n' +
+            'Elicitação de requisitos de software para as novas funcionalidades, análise e projeto das novas funcionalidades identificadas;\n\n' +
+            'Desenvolvimento do aplicativo;\n\n' +
+            'Testes de software;\n\n' +
+            'Tecnologias: Java, Kotlin, PostgreSQL.\n'
+          }
         />
       </div>
       <div className="flex items-center justify-center">
@@ -68,9 +104,11 @@ export default function About() {
         <Basico hardSkill="C++" />
         <Especialista hardSkill="CSS" />
         <Avancado hardSkill="Cypress" />
+        <Intermediario hardSkill="Dart" />
         <Especialista hardSkill="Docker" />
         <Especialista hardSkill="Expo" />
         <Especialista hardSkill="Figma" />
+        <Intermediario hardSkill="Flutter" />
         <Especialista hardSkill="Git" />
         <Especialista hardSkill="Groovy" />
         <Especialista hardSkill="HTML" />
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -7,37 +7,39 @@ import Copyright from '@/components/Copyright'
 
 export default function Projetos() {
   return (
-    <div className="flex h-screen flex-col">
-      <div className="mt-24 justify-center px-12 md:px-52">
-        <h1 className="text-5xl">Meus Projetos</h1>
-        <p className="font-sans">
-          Estes são alguns dos trabalhos e projetos que já realizei.
-        </p>
-      </div>
-      <div className="ml-12 mt-12 flex flex-col font-sans md:ml-52 md:mr-52 md:flex-row">
-        <div className="mb-4 flex flex-col md:mr-4">
-          <a href="">
-            <CardProject imagem={ecoleta} />
+    <div className="flex h-screen">
+      <div className="flex-col">
+        <div className="mt-24 justify-center px-12 md:px-52">
+          <h1 className="text-5xl">Meus Projetos</h1>
+          <p className="font-sans">
+            Estes são alguns dos trabalhos e projetos que já realizei.
+          </p>
+        </div>
+        <div className="ml-12 mt-12 flex flex-col font-sans md:ml-52 md:mr-52 md:flex-row">
+          <div className="mb-4 flex flex-col md:mr-4">
+            <a href="">
+              <CardProject imagem={ecoleta} />
+              <p className="mt-2 w-[280px] px-2">
+                O Ecoleta é um marketplace que ajuda pessoas a encontrarem
+                pontos de coleta de resíduos de forma eficiente.
+              </p>
+            </a>
+          </div>
+          <div className="mb-4 flex flex-col md:mr-4">
+            <CardProject imagem={gobarber} />
             <p className="mt-2 w-[280px] px-2">
-              O Ecoleta é um marketplace que ajuda pessoas a encontrarem pontos
-              de coleta de resíduos de forma eficiente.
+              Um aplicativo de agendamento de barbeiros que permite aos usuários
+              escolher em uma lista de barbeiros e barbeiros para acompanhar
+              seus compromissos.
             </p>
-          </a>
-        </div>
-        <div className="mb-4 flex flex-col md:mr-4">
-          <CardProject imagem={gobarber} />
-          <p className="mt-2 w-[280px] px-2">
-            Um aplicativo de agendamento de barbeiros que permite aos usuários
-            escolher em uma lista de barbeiros e barbeiros para acompanhar seus
-            compromissos.
-          </p>
-        </div>
-        <div className="mb-4 flex flex-col md:mr-4">
-          <CardProject imagem={proffy} />
-          <p className="mt-2 w-[280px] px-2">
-            Um aplicativo para encontrar alunos que querem aprender ou
-            professores para ensinar, sem limitações de área.
-          </p>
+          </div>
+          <div className="mb-4 flex flex-col md:mr-4">
+            <CardProject imagem={proffy} />
+            <p className="mt-2 w-[280px] px-2">
+              Um aplicativo para encontrar alunos que querem aprender ou
+              professores para ensinar, sem limitações de área.
+            </p>
+          </div>
         </div>
       </div>
       <Copyright />
